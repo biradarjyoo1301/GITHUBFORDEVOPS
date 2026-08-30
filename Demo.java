2aa3a7da56eaad0bc0546da15919592a12410c19
biradarjyoo1301 <biradarjyoo1301@gmail.com>
2026-08-30 15:28:17 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,12 +2,10 @@ public class Demo {
 
     
         public static void main(String[] args) {
-<<<<<<< HEAD
-            System.out.println("hiiiigit");
-=======
+
             System.out.println("hiiii");
              System.out.println("Helooo");
->>>>>>> cb4cc9780658e27b51376f43f125afdecb9a6a23
+
         
     }
     
